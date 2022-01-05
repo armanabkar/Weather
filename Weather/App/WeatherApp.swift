eda02c88083130582989e8f997bba5d09ee564f9
Arman Abkar <armanabkar@gmail.com>
2022-01-05 14:26:27 +0330

    initial commit

---
+++ Weather/App/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Arman Abkar on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
